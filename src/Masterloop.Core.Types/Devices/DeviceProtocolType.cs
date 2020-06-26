0af8e7915eb126dfcae02d57184d3830beb972c8
Jan Helge Skailand <jhs@masterloop.com>
2020-06-26 06:30:50 +0200

    Extended CommandResponse with meta information, DeviceProtocolType, DeviceConnection structures.

---
+++ src/Masterloop.Core.Types/Devices/DeviceProtocolType.cs
@@ -0,0 +1,10 @@
+﻿namespace Masterloop.Core.Types.Devices
+{
+    public enum DeviceProtocolType
+    {
+        None = 0,
+        HTTP = 1,
+        AMQP = 2,
+        MQTT = 3
+    }
+}
---
+++ src/Masterloop.Core.Types/LiveConnect/DeviceConnection.cs
@@ -0,0 +1,14 @@
+﻿using Masterloop.Core.Types.Devices;
+using Masterloop.Core.Types.Settings;
+using System;
+
+namespace Masterloop.Core.Types.LiveConnect
+{
+    public class DeviceConnection
+    {
+        public DateTime ServerTime { get; set; }
+        public DeviceProtocolType Protocol { get; set; }
+        public DeviceNode Node { get; set; }
+        public SettingsPackage Settings { get; set; }
+    }
+}
