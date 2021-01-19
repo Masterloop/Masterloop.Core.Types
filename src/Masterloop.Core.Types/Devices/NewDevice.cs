0ad92a5799f4b78a28aeea3bd6e5d6eae4e1ac18
Jan Helge Skailand <jhs@masterloop.com>
2021-01-19 13:07:07 +0100

    Added NewDevice DTO for additional properties in CreateDevice method.

---
+++ src/Masterloop.Core.Types/Devices/NewDevice.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Masterloop.Core.Types.Devices
+{
+    public class NewDevice
+    {
+        public string TemplateId { get; set; }
+
+        public string MID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string PreSharedKey { get; set; }
+
+        public DateTime? CreatedOn { get; set; }
+
+        public DateTime? UpdatedOn { get; set; }
+    }
+}
